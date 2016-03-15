dd671100aa5e00fe62b650fb2f2b037c9ec15306
Roboticas Student <fmiranda@gmail.com>
2016-03-15 09:47:18 -0300

    modificacao do exemplo python

---
+++ exemplo_python/scripts/exemplo_python_code.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from math import fabs
+
+
+velocidade_objetivo = Twist();
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=3)
+
+def notificacao(data):
+    """
+        Codigo de notificacao executado sempre que chega uma leitura da odometria
+
+        Esta leitura chega na variavel data e e'  um objeto do tipo odometria
+    """
+    # Todo: a partir de uma leitura da odometria faça
+    # um publish na velocidade até que o robô tenha andado 2 metros
+    
+    velocidade_objetivo = Twist()
+    velocidade_objetivo.linear.x = 0
+    velocidade_objetivo.linear.y = 0
+    velocidade_objetivo.linear.z = 0
+
+
+
+
+
+def controle():
+    """
+        Função inicial do programa
+    """
+    rospy.init_node('Exemplo_Python')
+    rospy.Subscriber('/odom', Odometry, notificacao)
+    # Initial movement.    
+    pub.publish(velocidade_objetivo)
+    rospy.spin() # Faz um loop infinito para o ROS nao retornar
+
+
+if __name__ == '__main__':
+    try:
+        controle()
+    except rospy.ROSInterruptException:
+        pass
