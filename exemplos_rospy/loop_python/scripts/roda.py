e1b58b4bffa94d08ec9d25e283cbf01ee5a5eea7
Fabio R. de Miranda <fmiranda@gmail.com>
2016-03-17 04:38:44 -0300

    Correcao do rosrun init em roda.py

--- exemplos_rospy/loop_python/scripts/roda.py
+++ exemplos_rospy/loop_python/scripts/roda.py
@@ -4,17 +4,17 @@
 import rospy
 from geometry_msgs.msg import Twist, Vector3
 
-v = 1  # Velocidade linear
-w = 0.5  # Velocidade angular
+v = 10  # Velocidade linear
+w = 5  # Velocidade angular
 
 if __name__ == "__main__":
-    rospy.init_node("roda.py")
+    rospy.init_node("roda_exemplo")
     pub = rospy.Publisher("cmd_vel", Twist, queue_size=3)
 
     try:
         while not rospy.is_shutdown():
             vel = Twist(Vector3(v,0,0), Vector3(0,0,w))
             pub.publish(vel)
-            rospy.sleep(1.0)
+            rospy.sleep(2.0)
     except rospy.ROSInterruptException:
         print("Ocorreu uma exceção com o rospy")
