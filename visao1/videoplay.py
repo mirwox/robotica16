cc3d7c00996ae23a1a9667251352f1877d59b754
Fabio de Miranda <fmiranda@gmail.com>
2016-04-28 07:43:28 -0300

    material para aula 2

--- visao1/videoplay.py
+++ visao1/videoplay.py
@@ -7,10 +7,10 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
